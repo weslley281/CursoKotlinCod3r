4c3899f62b0d3d334009ff0b9effacfd22552324
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-10-30 07:05:29 -0400

    a

---
+++ fundamentos/teteImport.kt
@@ -0,0 +1,10 @@
+package fundamentos
+
+import fundamentos.pacoteA.simplesFuncao as funcaoSimples
+import fundamentos.pacoteA.Coisa
+import fundamentos.pacoteA.FaceMoeda.CARA
+import fundamentos.pacoteB.*
+
+fun main(args: Array<String>){
+    kotlin.io.println(funcaoSimples("OK"))
+}
