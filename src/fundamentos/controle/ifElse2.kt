058b578a42d2f37f8753929c565cf13f778255b3
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-10-31 06:45:46 -0400

    ss

---
+++ src/fundamentos/controle/ifElse2.kt
@@ -0,0 +1,10 @@
+package fundamentos.controle
+
+fun main(args: Array<String>) {
+    val n1: Int = 16
+    val n2: Int = 3
+
+    val maiorValor = if (n1 > n2) n1 else n2
+
+    println("O maior valo é $maiorValor")
+}
