a6fb4f0f8d4234443e63dec980469db79815b06e
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2022-10-31 11:58:47 -0400

    a

---
+++ src/fundamentos/controle/tabuada.kt
@@ -0,0 +1,9 @@
+package fundamentos.controle
+
+fun main(args: Array<String>) {
+//    print("Digite o numero da tabuada: ")
+//    var tabuada = readLine()
+//    var numeroTabuada = tabuada?.toInt()
+//
+//    for (operador = 1; operdador++ )
+}
