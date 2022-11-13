9e023be889f88ed68a74487652ece5c0d6893cf3
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-11-13 18:01:58 -0400

    a

---
+++ src/classes/construtor1.kt
@@ -0,0 +1,18 @@
+package classes
+
+class Filme{
+    val nome: String
+    val anoLancamento: Int
+    val genero: String
+
+    constructor(nome: String, anoLancamento: Int, genero: String){
+        this.nome = nome
+        this.anoLancamento = anoLancamento
+        this.genero = genero
+    }
+}
+
+fun main(args: Array<String>) {
+    val filme = Filme("Wakanda Forever", 2022, "Ação")
+    println("O filme ${filme.nome} do genero ${filme.genero} foi lançado em ${filme.anoLancamento}")
+}
