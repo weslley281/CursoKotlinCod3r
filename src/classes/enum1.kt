c5c75bc62179a769db0eda1b6d8420b8f8efa0c1
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-11-16 06:40:35 -0400

    a

---
+++ src/classes/enum1.kt
@@ -0,0 +1,15 @@
+package classes
+
+enum class DiaSemana{
+    DOMINGO,
+    SEGUNDA,
+    TERCA,
+    QUARTA,
+    QUINTA,
+    SEXTA,
+    SABADO
+}
+
+fun main(args: Array<String>) {
+    println("O melhor dia da semana é ${DiaSemana.DOMINGO}")
+}
---
+++ src/lambdas/lambda2.kt
@@ -0,0 +1,3 @@
+package lambdas
+
+class Calculadora
