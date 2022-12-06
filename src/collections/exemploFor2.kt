8d25eb383e9fe45fb39813e742a2b8cdbe3b62b5
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-12-06 06:45:38 -0400

    a

---
+++ src/collections/exemploFor2.kt
@@ -0,0 +1,7 @@
+package collections
+
+fun main(args: Array<String>) {
+    val alunos = arrayListOf("Fulano", "Cicrano", "Deltrano", "Beltrana", "Maltrano")
+
+    alunos.forEach { println(it) }
+}
